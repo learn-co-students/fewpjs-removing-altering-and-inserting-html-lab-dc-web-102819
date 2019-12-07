db29972d4c6a82bf40857dea1825ae112de14ac6
github username <github email address>
2019-12-07 09:51:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+// let element = document.createElement('div')
+
+
+const myString = "Hello World" ; 
+
+document.querySelector("main#main").remove()
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory" ;
+newHeader.innerHTML = "Flatiron School is the champion!" ;
+newHeader.className = "Victory" ; 
+
+
+
+
